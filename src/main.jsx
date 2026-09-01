98fc244d97c3ce26717cf093d1f1c8c56303b30d
Vyas Devgna <141345367+vyas-devgna@users.noreply.github.com>
2026-09-01 09:40:21 +0530

    ui: load viewport topbar overrides

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client'
 import 'maplibre-gl/dist/maplibre-gl.css'
 import './styles.css'
 import './enhancements.css'
+import './topbar.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(<App/>)
