00970443f13f283e7d8874261ca5451ee4547913
Vyas Devgna <141345367+vyas-devgna@users.noreply.github.com>
2026-09-12 12:34:52 +0530

    refactor: drop removed app guide stylesheet

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,6 @@ import './bottom-dock.css'
 import './map-upgrade.css'
 import './stay-planning.css'
 import './branch-board.css'
-import './app-guide.css'
 import {data} from './lib.js'
 import App from './App.jsx'
 
